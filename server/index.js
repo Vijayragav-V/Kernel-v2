8fe51bc3098ed1cb0ce0a95f2f4c1a2276bb003b
Vijay Varadarajan <151465767+Vijayragav-V@users.noreply.github.com>
2024-01-07 01:55:05 +0000

    Worked on server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const ecRoute = require("./extracurricular.routes");
+
+const router = express.Router();
+
+router.use("/extracurricular", exRoute);
+
+router.use((req, res) => {
+  res.status(404).send({ message: "Not Found" });
+});
+
+module.exports = router;
