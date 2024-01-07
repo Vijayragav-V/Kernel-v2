f32bfcc664c56463d617394f80ede95233ac7cd2
Vijay Varadarajan <151465767+Vijayragav-V@users.noreply.github.com>
2024-01-06 21:03:41 -0500

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const ecRoute = require("./extracurricular.routes");
 
+const router = express.Router();
+
+router.use("/extracurricular", ecRoute);
+
+router.use((req, res) => {
+  res.status(404).send({ message: "Not Found" });
+});
+
+module.exports = router;
